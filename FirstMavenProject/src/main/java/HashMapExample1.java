a4fa314830cbbf1f6d9b9742ff922dfaccec0d32
jeno-mathew <jeno.mathew@techversantinfotech.com>
2023-07-04 20:35:06 +0530

    excel read codes new

--- FirstMavenProject/src/main/java/HashMapExample1.java
+++ FirstMavenProject/src/main/java/HashMapExample1.java
@@ -5,7 +5,7 @@ public class HashMapExample1{
 		map.put(1,"Mango");  //Put elements in Map  
 		map.put(2,"Apple");    
 		map.put(3,"Banana");   
-		map.put(4,"Grapes");   
+		map.put(4,"Grapes");    
 
 		System.out.println("Iterating Hashmap...");  
 		for(Map.Entry m : map.entrySet()){    
